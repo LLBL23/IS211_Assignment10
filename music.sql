1ae7d8501c8d42241ed58c26d86a99149fbff835
LaToya <ltbowenslarson@gmail.com>
2024-03-31 17:48:05 -0500

    commit my changes to music

--- music.sql
+++ music.sql
@@ -1,1 +1,5 @@
+CREATE TABLE artist (id INT PRIMARY KEY, artist TEXT)
+CREATE TABLE album (id INT PRIMARY KEY, album_title TEXT, artist_id INT REFERENCES artist(id))
+CREATE TABLE songs (id INT PRIMARY KEY, song_title TEXT, album_id INT REFERENCES album(id), track_number INT, track_length INT)
+
 
